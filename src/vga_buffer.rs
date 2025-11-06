fbfdf19d595b066c3262b6d9f359c0ff4e392cfc
saltchicken <john.eicher89@gmail.com>
2025-11-06 15:45:16 -0800

    Text now green

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -225,7 +225,7 @@ lazy_static! {
     pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
         row_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        color_code: ColorCode::new(Color::Green, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
 }
